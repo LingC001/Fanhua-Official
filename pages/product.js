d5d76803f168a92236b745c8c7cb60550beefbfd
杨博 <bo.yang@marketingforce.com>
2023-10-25 20:49:30 +0800

    feat:产品介绍

--- pages/product.js
+++ pages/product.js
@@ -1,5 +1,16 @@
+import style from '@/pages/styles/products.module.scss'
 const Home = () => {
-  return <div>product</div>
+  return (
+    <div className={style.products}>
+       <div className="product-item">
+         <div className="pic"></div>
+          <div className="desc">
+            <div className="title"></div>
+            <div className="text"></div>
+          </div>
+       </div>
+    </div>
+  )
 }
 
 export default Home
