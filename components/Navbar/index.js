cd216318464008bc80c9603fa21423225a03c80d
yangbo <1070939301@qq.com>
2023-10-26 21:04:41 +0800

    fix：路由跳转问题

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -2,7 +2,11 @@ import style from './index.module.scss'
 import Image from 'next/image'
 import Link from 'next/link'
 import { useState } from 'react'
+import { useRouter } from 'next/router'
+
 const Navbar = () => {
+  const router = useRouter()
+
   const tabs = [
     {
       name: '首页',
@@ -32,9 +36,18 @@ const Navbar = () => {
 
   const tabClick = (index) => {
     setActive(index)
+    router.push(tabs[index].route)
   }
 
-  // 根据路径设置导航高亮
+  // 页面刷新时根据路径设置导航高亮
+  // 通过router对象获取当前路径
+  const { pathname } = router
+  // 通过路径获取当前导航索引
+  const activeIndexByPath = tabs.findIndex((i) => i.route === pathname)
+  // 如果当前导航索引不为-1，且与activeIndex不同，则设置activeIndex
+  if (activeIndexByPath !== -1 && activeIndexByPath !== activeIndex) {
+    setActive(activeIndexByPath)
+  }
 
   return (
     <div className={style.navbar}>
@@ -51,7 +64,7 @@ const Navbar = () => {
             className={tabClass(idx)}
             onClick={() => tabClick(idx)}
           >
-            <Link href={i.route}>{i.name}</Link>
+            {i.name}
           </span>
         ))}
       </div>
