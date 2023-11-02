f37d36f11f357ea2b59d26402cfaf67c7b78742f
yangbo <1070939301@qq.com>
2023-11-02 22:31:53 +0800

    feat:联系我们页面

--- pages/components/BaiduMap/BaiduMap.js
+++ pages/components/BaiduMap/BaiduMap.js
@@ -1,19 +1,23 @@
-import {LoadBmap} from "@/util";
-import {useEffect} from "react";
-
+import { LoadBmap } from '@/util'
+import { useEffect } from 'react'
 
 const BaiduMap = () => {
   useEffect(() => {
-    LoadBmap().then(BMap => {
-      let map = new BMap.Map("cont");
-      let point = new BMap.Point(116.404, 39.915);
-      map.centerAndZoom(point, 15);
+    LoadBmap().then((BMapGL) => {
+      let map = new BMapGL.Map('cont')
+      map.enableScrollWheelZoom(true)
+      let point = new BMap.Point(113.675934, 29.905305)
+      map.centerAndZoom(point, 16)
+      var marker = new BMapGL.Marker(point)
+      map.addOverlay(marker)
     })
-  }, []);
+  }, [])
   return (
-    <div id={'cont'} style={{height: '300px'}}>
-    </div>
+    <div
+      id={'cont'}
+      style={{ height: '500px', width: '1000px', margin: '0 auto' }}
+    ></div>
   )
-};
+}
 
-export default BaiduMap;
+export default BaiduMap
--- pages/contact.js
+++ pages/contact.js
@@ -1,12 +1,22 @@
-import Bmap from "@/pages/components/BaiduMap/BaiduMap";
+import style from '@/pages/styles/contact.module.scss'
+import { useState } from 'react'
+import BaiduMap from '@/pages/components/BaiduMap/BaiduMap'
 
+const Contact = () => {
+  const [isShowMap, setIsShowMap] = useState(false)
 
-const Home = () => {
   return (
-    <div>
-      <Bmap/>
+    <div className={style.contact}>
+      <div className="section-row">
+        <div className="title">联系人：杨涛</div>
+        <div className="title">联系电话：15972390611</div>
+        <div className="title" style={{ marginBottom: '24px' }}>
+          通讯地址：湖北省咸宁市嘉鱼县陆溪镇界石村
+        </div>
+        <BaiduMap />
+      </div>
     </div>
   )
 }
 
-export default Home
+export default Contact
